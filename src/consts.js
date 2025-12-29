a7967c86cd3257673a880bb565ce714b8c8a829f
Jeambo <226494145+jeambos@users.noreply.github.com>
2025-12-29 19:23:55 +0800

    feat: 更新网站标题和描述，提升品牌形象与内容相关性

--- src/consts.js
+++ src/consts.js
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "ʕ•ᴥ•ʔ Astro Bear Blog";
-export const SITE_DESCRIPTION = "Astro Bear Blog Theme!";
+export const SITE_TITLE = "Jeambo ( ๑•₃• )  ˗ˏˋ ♡ ˎˊ˗ ";
+export const SITE_DESCRIPTION = "关注非单偶制和多元关系的译者，写作者，中文多元关系文库站长。看到可爱狗狗会走不动路。";
 export const SITE_URL = "http://localhost:4321";
 export const SEARCH_URL = "https://duckduckgo.com"
