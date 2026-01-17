263cf14ce71be34289801c7324e609618b303e8c
Jeambo <226494145+jeambos@users.noreply.github.com>
2026-01-18 03:15:20 +0800

    修订projects内容和卡片样式

--- src/content/config.js
+++ src/content/config.js
@@ -28,6 +28,7 @@ const notes = defineCollection({
     title: z.string(),
     // 修正1：既然是笔记，按日期排序更合理，所以要有 pubDate
     pubDate: z.coerce.date(),
+	lastUpdated: z.coerce.date().optional(),
     // 修正2：加上可选的 description，防止页面读取时报错
     description: z.string().optional(),
   }),
